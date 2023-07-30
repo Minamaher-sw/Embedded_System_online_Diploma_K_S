7442995ca1c2181e03e379b958992e34da3129c4
Minamaher-sw <menamosadef5@gmail.com>
2023-07-30 19:57:26 +0300

    modify some comment

--- C_basics/assigment_1/Ex3_main.c
+++ C_basics/assigment_1/Ex3_main.c
@@ -16,19 +16,20 @@ int main()
 	setbuf(stdout ,NULL) ;
 
 	/* Local Variable */
+	//long is 4 byte has range (-2,147,483,648 to +2,147,483,647	)
 	//long long for integer is 8 byte has range from -9,223,372,036,854,775,808 through 9,223,372,036,854,775,807
 	long int First_Integer_Number_li =0 ;
 	long int Second_Integer_Number_li =0 ;
 	long long int Sum_lli =0 ;
 	/*___________________*/
 
-	// Request from user enter integer number
+	// Request from user enter first integer number
 	printf("please Enter First_Integer_Number ") ;
 
 	//to take from user Number
 	scanf("%ld" ,&First_Integer_Number_li) ;
 
-	// Request from user enter integer number
+	// Request from user enter second integer number
 	printf("\nplease Enter Second_Integer_Number ") ;
 
 	//to take from user Number

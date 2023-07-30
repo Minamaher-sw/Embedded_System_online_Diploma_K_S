db3b3d99a5cf8f00407400f0f74b16c2411ea391
Minamaher-sw <menamosadef5@gmail.com>
2023-07-30 19:14:33 +0300

    Code use to print for user C programing HW_1/EX1

---
+++ C_basics/assigment_1/Ex1_main.c
@@ -0,0 +1,28 @@
+/*******************************************
+	****   @File   : MAIN_EX.C      ***
+	****   Author  : Mina Maher     ***
+	****   Data    : 30/7/2023      ***
+	****   Version : v1             ***
+	****   Brief   : C Program to Print a Sentence on console  ***
+ ********************************************/
+
+/** Linking Section   */
+#include <stdio.h>
+
+/* Main section start */
+int main()
+{
+	//show Sentence to user
+	printf("C Programming");
+
+	return 0 ;
+}
+/* Main section end */
+
+/****************************************************
+   Name                    Date               Update
+ Mina Maher              30/7/2023            No
+  ****************************************************/
+
+
+

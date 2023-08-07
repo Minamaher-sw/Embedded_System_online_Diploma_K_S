6933c9aaeef4c0147affef57685ab691543eb6c1
Minamaher-sw <menamosadef5@gmail.com>
2023-08-07 23:30:23 +0300

    C Program to summation from 1 until number enter by user

---
+++ Lecture_labs/Lecture_one/While_loop/Lab1_main.c
@@ -0,0 +1,37 @@
+/*******************************************
+	****   @File   : LAB1_EX.C      ***
+	****   Author  : Mina Maher     ***
+	****   Data    : 7/8/2023      ***
+	****   Version : v1             ***
+	****   Brief   : C Program to summation from 1 until number enter by user   ***
+ ********************************************/
+/** Linking Section   */
+#include <stdio.h>
+
+/* Main section start */
+int main()
+{
+	setbuf(stdout ,NULL) ;
+	/*local variables */
+	long int Number_Iterations_l ;
+	long int Counter_l =1;
+	long long int sum_ll =0;
+	//ask user to enter number that want to print hello world
+	printf("Enter a number ") ;
+	scanf("%ld" ,&Number_Iterations_l) ;
+	while(Counter_l <= Number_Iterations_l)
+	{
+		sum_ll += Counter_l ;
+		Counter_l++;
+	}
+	printf("summation from 1 to %ld is %lld" ,Number_Iterations_l ,sum_ll) ;
+	return 0 ;
+}
+/* Main section end */
+
+/****************************************************
+   Name                    Date               Update
+ Mina Maher              7/8/2023            No
+  ****************************************************/
+
+

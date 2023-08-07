554d353f5070a7c3d3766e076d511417b2ceac69
Minamaher-sw <menamosadef5@gmail.com>
2023-08-07 23:22:43 +0300

    C Program to find the largest value of the three given values

--- Lecture_labs/Lecture_one/if_condition/Lab2_main.c
+++ Lecture_labs/Lecture_one/if_condition/Lab2_main.c
@@ -10,41 +10,41 @@
 #include <stdio.h>
 
 /* Main section start */
-//int main()
-//{
-//	setbuf(stdout ,NULL) ;
-//    /*local variable */
-//	double _First_Number_d  ;
-//	double _Second_Number_d ;
-//	double _Third_Number_d  ;
-//	/*______________________*/
-//
-//	printf("Enter three values") ;
-//	scanf("%lf%lf%lf" ,&_First_Number_d ,&_Second_Number_d ,& _Third_Number_d );
-//
-//	if( _First_Number_d > _Second_Number_d )
-//	{
-//		if(_First_Number_d > _Third_Number_d)
-//		{
-//			printf("Largest number is %lf" , _First_Number_d);
-//		}
-//		else
-//		{
-//			printf("Largest number is %lf" , _Third_Number_d);
-//		}
-//	}
-//	else if((_Second_Number_d > _First_Number_d ))
-//	{
-//		if(_Second_Number_d > _Third_Number_d)
-//		{
-//			printf("Largest number is %lf" , _Second_Number_d);
-//		}
-//		else
-//		{
-//			printf("Largest number is %lf" , _Third_Number_d);
-//		}
-//	}
-//}
+int main()
+{
+	setbuf(stdout ,NULL) ;
+    /*local variable */
+	double _First_Number_d  ;
+	double _Second_Number_d ;
+	double _Third_Number_d  ;
+	/*______________________*/
+
+	printf("Enter three values") ;
+	scanf("%lf%lf%lf" ,&_First_Number_d ,&_Second_Number_d ,& _Third_Number_d );
+
+	if( _First_Number_d > _Second_Number_d )
+	{
+		if(_First_Number_d > _Third_Number_d)
+		{
+			printf("Largest number is %lf" , _First_Number_d);
+		}
+		else
+		{
+			printf("Largest number is %lf" , _Third_Number_d);
+		}
+	}
+	else if((_Second_Number_d > _First_Number_d ))
+	{
+		if(_Second_Number_d > _Third_Number_d)
+		{
+			printf("Largest number is %lf" , _Second_Number_d);
+		}
+		else
+		{
+			printf("Largest number is %lf" , _Third_Number_d);
+		}
+	}
+}
 
 /* Main section end */
 

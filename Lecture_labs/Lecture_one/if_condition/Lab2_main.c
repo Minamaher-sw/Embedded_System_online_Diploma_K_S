b6073a3babdf8a2a8b39add0684c176b8ca2cfba
Minamaher-sw <menamosadef5@gmail.com>
2023-08-07 23:21:39 +0300

    C Program to find the largest value of the three given values

---
+++ Lecture_labs/Lecture_one/if_condition/Lab2_main.c
@@ -0,0 +1,57 @@
+/*******************************************
+	****   @File   : LAB2_EX.C      ***
+	****   Author  : Mina Maher     ***
+	****   Data    : 7/8/2023      ***
+	****   Version : v1             ***
+	****   Brief   : C Program to find the largest value of the three given values.
+  ***
+ ********************************************/
+/** Linking Section   */
+#include <stdio.h>
+
+/* Main section start */
+//int main()
+//{
+//	setbuf(stdout ,NULL) ;
+//    /*local variable */
+//	double _First_Number_d  ;
+//	double _Second_Number_d ;
+//	double _Third_Number_d  ;
+//	/*______________________*/
+//
+//	printf("Enter three values") ;
+//	scanf("%lf%lf%lf" ,&_First_Number_d ,&_Second_Number_d ,& _Third_Number_d );
+//
+//	if( _First_Number_d > _Second_Number_d )
+//	{
+//		if(_First_Number_d > _Third_Number_d)
+//		{
+//			printf("Largest number is %lf" , _First_Number_d);
+//		}
+//		else
+//		{
+//			printf("Largest number is %lf" , _Third_Number_d);
+//		}
+//	}
+//	else if((_Second_Number_d > _First_Number_d ))
+//	{
+//		if(_Second_Number_d > _Third_Number_d)
+//		{
+//			printf("Largest number is %lf" , _Second_Number_d);
+//		}
+//		else
+//		{
+//			printf("Largest number is %lf" , _Third_Number_d);
+//		}
+//	}
+//}
+
+/* Main section end */
+
+/****************************************************
+   Name                    Date               Update
+ Mina Maher              30/7/2023            No
+  ****************************************************/
+
+
+
